3b82615654069cc7374de148c423ea04d52df224
lazytia <57658400+lazytia@users.noreply.github.com>
2020-03-20 00:49:48 +0900

    ㅋ

--- lib/record/my_record.dart
+++ lib/record/my_record.dart
@@ -5,13 +5,13 @@ import 'package:http/http.dart' as http;
 import 'package:lol_pr_splash/j_main.dart';
 import 'package:flutter/src/widgets/text.dart';
 
-class URLS{
+// class URLS{
   
-  static const String L_URL = "https://loger-django.herokuapp.com/my_info/?id='+'Name'";
-}
-
+//   static const String L_URL = "https://loger-django.herokuapp.com/my_info/?id='+'_handleSubmitted'";
+// }
+final summonerName = TextEditingController();
 class User {
-   final String summonerName;
+      final String summonerName;
       final String summonerLevel;
       final String code;
   //  final String solrank;
@@ -21,19 +21,20 @@ class User {
   final Map<String, dynamic> lol;
   User({this.summonerName,  this.summonerLevel, this.code, this.lol});
   factory User.fromJson(Map<String, dynamic> userMap) {
-    return User(
+    var user = User(
       summonerName: userMap['summonerName'],
       summonerLevel: userMap['summonerLevel'],
       code: userMap['code'],
       
     );
+    return user;
   }
 }
 
 
 Future<User> fetchUser() async {
   
-  final response = await http.get('https://loger-django.herokuapp.com/my_info/?id='+'Name');
+  final response = await http.get('https://loger-django.herokuapp.com/my_info/?id='+'_textController');
     if (response.statusCode == 200) {
       final userMap = json.decode(response.body);
       return User.fromJson(userMap);
@@ -52,31 +53,44 @@ class MyRecord extends StatelessWidget {
     final style = TextStyle(fontSize: 21, height: 2.0);
     void _handleSubmitted(String text){
       _textController.clear(); }
-    return Material(
-      child: Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(55.0),
-          child: TextField(
-            textAlign: TextAlign.center,
-            controller: _textController,
+    return Scaffold(
+        body: SingleChildScrollView(
+          child: Center(
+          child: Column(
+            children: <Widget>[
+ 
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text('적어봐 소환사', 
+                       style: TextStyle(fontSize: 22))),
+ 
+              Container(
+              width: 280,
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                  
+                  autocorrect: true,
+                  decoration: InputDecoration(hintText: '소환사 이름'),
+                )
+              ),
+      
+    
+      //  Container( 
+      //    margin: const EdgeInsets.symmetric(
+      //      horizontal: 4.0), 
+      //      child: IconButton( 
+      //        icon: Icon(Icons.send), 
+      //        onPressed: () => _handleSubmitted(_textController.text),
+      //        ),
+      //  ),
+           RaisedButton(
+                onPressed: () => _handleSubmitted(_textController.text),
             
-            decoration: InputDecoration(
-          	border: OutlineInputBorder(),
-    	      labelText: '소환사 이름',
-            )
-          ),
-        ),
-       
-       Container( 
-         margin: const EdgeInsets.symmetric(
-           horizontal: 4.0), 
-           child: IconButton( 
-             icon: Icon(Icons.send), 
-             onPressed: () => _handleSubmitted(_textController.text),
-             ),
-       ),
-
+                color: Colors.pink,
+                textColor: Colors.white,
+                padding: EdgeInsets.fromLTRB(8, 8, 8, 8),
+                child: Text('검색'),
+              ),
 
 
         FutureBuilder(
@@ -105,7 +119,7 @@ class MyRecord extends StatelessWidget {
         ),
        ],
       ),
-    );
+    )));
    
   }
 }
