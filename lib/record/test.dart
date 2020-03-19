479ede7793ce13f3206608a54de0e914f4f260ed
lazytia <57658400+lazytia@users.noreply.github.com>
2020-03-20 00:48:41 +0900

    알아버렸다

--- lib/record/test.dart
+++ lib/record/test.dart
@@ -72,26 +72,27 @@ class TransfterDataWidget extends State {
  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SingleChildScrollView(
-          child: Center(
-          child: Column(
-            children: <Widget>[
+    
+    // return Scaffold(
+    //     body: SingleChildScrollView(
+    //       child: Center(
+    //       child: Column(
+    //         children: <Widget>[
  
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Text('적어봐 소환사', 
-                       style: TextStyle(fontSize: 22))),
+    //           Padding(
+    //             padding: const EdgeInsets.all(12.0),
+    //             child: Text('적어봐 소환사', 
+    //                    style: TextStyle(fontSize: 22))),
  
-              Container(
-              width: 280,
-              padding: EdgeInsets.all(10.0),
-              child: TextField(
+    //           Container(
+    //           width: 280,
+    //           padding: EdgeInsets.all(10.0),
+    //           child: TextField(
                   
-                  autocorrect: true,
-                  decoration: InputDecoration(hintText: '소환사 이름'),
-                )
-              ),
+    //               autocorrect: true,
+    //               decoration: InputDecoration(hintText: '소환사 이름'),
+    //             )
+    //           ),
 
  
               // Container(
@@ -126,7 +127,7 @@ class TransfterDataWidget extends State {
                 textColor: Colors.white,
                 padding: EdgeInsets.fromLTRB(8, 8, 8, 8),
                 child: Text('검색'),
-              ),
+              );
  
               // Visibility(
               //   visible: visible, 
@@ -136,8 +137,8 @@ class TransfterDataWidget extends State {
               //     )
               //   ),
  
-            ],
-          ),
-        )));
+        //     ],
+        //   ),
+        // )));
   }
 }
